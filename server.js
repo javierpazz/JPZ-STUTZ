4c84643127c147b43805df173008a7e27391b09e
JAVIER <javierpia0105@gmail.com>
2024-10-14 19:02:01 -0300

    6

--- server.js
+++ server.js
@@ -38,7 +38,7 @@ console.log(`"${process.env.MONGODB_URI}"`);
 mongoose
   // .connect(process.env.MONGODB_URI)
   // .connect(`"${process.env.MONGODB_URI}"`)
-  .connect("mongodb+srv://admin:admin123456@chatapp.fe0krcf.mongodb.net/sdb")
+  .connect("mongodb+srv://admin:admin123456@stutzapp.nf44trs.mongodb.net/sdb")
   .then(() => {
     console.log('connected to db');
   })
